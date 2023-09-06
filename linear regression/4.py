8af57b05ad786feb47d25a07deef68a6dd40d76a
ori levi <orilev989@gmail.com>
2023-09-06 12:43:18 +0000

    Update file 4.py

--- linear regression/4.py
+++ linear regression/4.py
@@ -9,39 +9,27 @@ DEGREE = 2
 
 def gradiant_decent(weight, arrX, arrY):
     gWeight = np.zeros(DEGREE + 1)
-    print(gWeight)
     n = len(arrY)
-    for i in range(n):
-        x = arrX[i]
-        y = arrY[i]
-        
-        gWeight[2] += x[2] * (y - np.dot(x, weight))
-        gWeight[1] += x[1] * (y - np.dot(x, weight))
-        gWeight[0] += (y - np.dot(x, weight))
-    gWeight *= -(2/n)
-    newWeight = updateWeights(weight, gWeight, 0.001)
+    
+    gWeight = -(2/n) * np.dot(arrX.T, (arrY - np.dot(arrX, weight)))
+    #print(weight)
+    newWeight = weight - gWeight * 0.001
     return newWeight
 
-def updateWeights(newWeight, gWeight, learningRate):
-    for i in range(len(newWeight)):
-        newWeight[i] = newWeight[i] - gWeight[i] * learningRate
-    return newWeight
 
-def singleCost(weight, x, y):
-    return (y - (weight[0] + weight[1] * x[1] + weight[2] * x[2]))
+def cost(weight,arrX, arrY):
+    return sum(arrY - np.dot(arrX, weight)) ** 2
 
-def cost(weight,data, y):
-    cost = 0
-    i = 0
-    for a in data:
-        cost+=  weight @ x ** 2
-        i += 1
-    return cost
+def guess(weight, x):
+    y = []
+    for i in range(len(x)):
+        y.append(weight[0] + weight[1] * x + weight[2] * x*x)
+    return y
 
 costs = []
 weight = np.ones(3)
 bias = 0
-epochs = 30
+epochs = 0
 x = np.load("Exercise 1 data-20230826\\TA_Xhouses.npy")
 y = np.load("Exercise 1 data-20230826\\TA_yprice.npy")
 
@@ -55,23 +43,23 @@ xfitPoly = PolynomialFeatures.transform(poly, xfit)
 yfit = model.predict(xfitPoly)
 #SkLearn
 i = 0
-new_x = []
-for num in x:
-    
-    new_x.append([np.float64(1)])
-    for j in range(DEGREE):
-        new_x[i].append(num ** (j +1))
-    i += 1
-print(new_x)
+new_x = np.ones((len(x)))
+for i in range(DEGREE):
+    new_x = np.c_[new_x, x**i]
+arrY = []
+for i in range(len(y)):
+    arrY.append(y[i][0])
+y = np.array(arrY)
 for i in range(epochs):
-    print(weight)
     weight = gradiant_decent(weight, new_x, y)
+    print(cost(weight, new_x, y))
     
 plt.scatter(x, y)
 plt.plot(xfit, yfit)
 plt.show()
-
+ 
 plt.clf()
 plt.scatter(x, y)
+
 plt.plot(list(range(4, 12)), [weight[0] + weight[1] * i + weight[2] * i ** 2 for i in range(4, 12)])
 plt.show()
