14b99ed7dff3d88219c768360620364b01fb9800
ori levi <orilev989@gmail.com>
2023-09-06 16:56:02 +0000

    Update file 4.py

--- linear regression/4.py
+++ linear regression/4.py
@@ -12,8 +12,9 @@ def gradiant_decent(weight, arrX, arrY):
     n = len(arrY)
     
     gWeight = -(2/n) * np.dot(arrX.T, (arrY - np.dot(arrX, weight)))
-    #print(weight)
-    newWeight = weight - gWeight * 0.001
+    print(weight)
+    print(gWeight)
+    newWeight = weight - gWeight * 0.00001
     return newWeight
 
 
@@ -21,15 +22,17 @@ def cost(weight,arrX, arrY):
     return sum(arrY - np.dot(arrX, weight)) ** 2
 
 def guess(weight, x):
-    y = []
+    print(x)
+    y = np.zeros(len(x))
+    print(y)
     for i in range(len(x)):
-        y.append(weight[0] + weight[1] * x + weight[2] * x*x)
+        np.insert(y[i],   weight[0] + weight[1] * x + weight[2] * x*x, i)
     return y
 
 costs = []
 weight = np.ones(3)
 bias = 0
-epochs = 0
+epochs = 30000
 x = np.load("Exercise 1 data-20230826\\TA_Xhouses.npy")
 y = np.load("Exercise 1 data-20230826\\TA_yprice.npy")
 
@@ -42,17 +45,18 @@ xfit = np.linspace(0,15, 100)[:, np.newaxis]
 xfitPoly = PolynomialFeatures.transform(poly, xfit)
 yfit = model.predict(xfitPoly)
 #SkLearn
-i = 0
+new_x = []
 new_x = np.ones((len(x)))
-for i in range(DEGREE):
+for i in range(1, DEGREE +1):
     new_x = np.c_[new_x, x**i]
+print(new_x)
 arrY = []
 for i in range(len(y)):
     arrY.append(y[i][0])
 y = np.array(arrY)
 for i in range(epochs):
     weight = gradiant_decent(weight, new_x, y)
-    print(cost(weight, new_x, y))
+    #print(cost(weight, new_x, y))
     
 plt.scatter(x, y)
 plt.plot(xfit, yfit)
@@ -60,6 +64,5 @@ plt.show()
  
 plt.clf()
 plt.scatter(x, y)
-
-plt.plot(list(range(4, 12)), [weight[0] + weight[1] * i + weight[2] * i ** 2 for i in range(4, 12)])
+plt.plot(list(range(4, 16)), [weight[0] + weight[1] * i + weight[2] * i ** 2 for i in range(4, 16)])
 plt.show()
