b4a3ba2b66d1bb57b27d0013f541ea6215a408bb
oriLevi <orilev989@gmail.com>
2024-03-07 19:48:03 +0200

    added some stuff

--- gui.py
+++ gui.py
@@ -1,36 +1,50 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
-from PyQt5.QtMultimedia import *
-from PyQt5.QtMultimediaWidgets import *
-import os 
-import sys 
-import time 
+import sys
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QApplication
+from PyQt5.QtGui import QPixmap, QImage
+from camera import *
 
-class App(QMainWindow):
-
+class ImageWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.title = 'test'
-        self.left = 50
-        self.top = 50
-        self.width = 400
-        self.height = 140
-        self.initUI()
+        self.image_label = QLabel()
+        self.image_label.setScaledContents(True)
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.textbox = QLineEdit(self)
-        self.textbox.move(150, 90)
-        self.textbox.resize(80,40)
-        
-        self.show()
-        
-    def mainLoop():
-        
-    
+        self.central_widget = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(self.image_label)
+        self.central_widget.setLayout(layout)
 
+        self.setCentralWidget(self.central_widget)
+        self.setWindowTitle("Image Viewer")
 
-if __name__ == '__main__':
+        # Connect the resize event to update the image
+        self.resizeEvent = self.update_image_size
+
+    def update_image(self, image_array):
+        """Updates the displayed image using a NumPy array.
+
+        Args:
+            image_array: A NumPy array representing the image data.
+        """
+
+        # Convert NumPy array to QImage
+        qimage = QImage(image_array.data, image_array.shape[1], image_array.shape[0], 
+                        image_array.strides[0])
+
+        # Convert QImage to QPixmap and set on the label
+        pixmap = QPixmap(qimage).scaled(self.size(), Qt.AspectRatioMode.KeepAspectRatio, Qt.SmoothScaling)
+        self.image_label.setPixmap(pixmap)
+
+    def update_image_size(self, event):
+        """Updates the image size based on the window size."""
+        if self.image_label.pixmap():
+            self.update_image(self.image_label.pixmap().toImage().copy())
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+    window = ImageWindow()
+    window.show()
+    while True:
+        id = input_to_model(calc_sleep(get_fps(), get_fps()-1),window)  
+        sys.exit(app.exec_())
+    
